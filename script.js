2bd5d7a94aab2e506843a35270d7e809cce23b77
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-11-04 18:04:30 +0700

    Added java script files

---
+++ script.js
@@ -0,0 +1,122 @@
+function startGame() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("game").style.display = "flex";
+}
+
+let botChoseArray = ["rock", "paper", "scissors"];
+let countYou = 0;
+let countBot = 0;
+
+function onClickRock() {
+    botIndex = Math.floor(Math.random()*3);
+    botChose = botChoseArray[botIndex];
+    document.getElementById("img-you").src = "images/rock.png";
+    if (botChose == "rock") {
+        // Draw
+        document.getElementById("img-bot").src ="images/rock.png";
+        document.getElementById("winner").innerHTML = "Draw";
+    } else if (botChose == "paper") {
+        // Lose
+        countBot += 1;
+        document.getElementById("score-bot").innerHTML = countBot;
+        document.getElementById("img-bot").src = "images/paper.png";
+        document.getElementById("winner").innerHTML = "Winner: Bot";
+    } else {
+        // Win
+        countYou += 1;
+        document.getElementById("score-you").innerHTML = countYou;
+        document.getElementById("img-bot").src = "images/scissors.png";
+        document.getElementById("winner").innerHTML = "Winner: You";
+    }
+    checkWinner()
+}
+
+function onClickPaper() {
+    botIndex = Math.floor(Math.random()*3);
+    botChose = botChoseArray[botIndex];
+    document.getElementById("img-you").src = "images/paper.png";
+    if (botChose == "rock") {
+        // Win
+        countYou += 1;
+        document.getElementById("score-you").innerHTML = countYou;
+        document.getElementById("img-bot").src ="images/rock.png";
+        document.getElementById("winner").innerHTML = "Winner: You";
+    } else if (botChose == "paper") {
+        // Draw
+        document.getElementById("img-bot").src = "images/paper.png";
+        document.getElementById("winner").innerHTML = "Draw";
+    } else {
+        // Lose
+        countBot += 1;
+        document.getElementById("score-bot").innerHTML = countBot;
+        document.getElementById("img-bot").src = "images/scissors.png";
+        document.getElementById("winner").innerHTML = "Winner: Bot";
+    }
+    checkWinner()
+}
+
+function onClickScissors() {
+    botIndex = Math.floor(Math.random()*3);
+    botChose = botChoseArray[botIndex];
+    document.getElementById("img-you").src = "images/scissors.png";
+    if (botChose == "rock") {
+        // Lose
+        countBot += 1;
+        document.getElementById("score-bot").innerHTML = countBot;
+        document.getElementById("img-bot").src ="images/rock.png";
+        document.getElementById("winner").innerHTML = "Winner: Bot";
+    } else if (botChose == "paper") {
+        // Win
+        countYou += 1;
+        document.getElementById("score-you").innerHTML = countYou;
+        document.getElementById("img-bot").src = "images/paper.png";
+        document.getElementById("winner").innerHTML = "Winner: You";
+    } else {
+        // Draw
+        document.getElementById("img-bot").src = "images/scissors.png";
+        document.getElementById("winner").innerHTML = "Draw";
+    }
+    checkWinner()
+}
+
+function checkWinner() {
+    if (countBot == 3) {
+        document.getElementById("choose-you").style.display = "none"
+        document.getElementById("winner").innerHTML = "Bot Win!"
+    } else if (countYou == 3) {
+        document.getElementById("choose-you").style.display = "none"
+        document.getElementById("winner").innerHTML = "You Win!"
+    }
+}
+
+function onClickMainScreen() {
+    document.getElementById("game").style.display = "none";
+    document.getElementById("home").style.display = "flex";
+    onClickReset()
+}
+function onClickReset() {
+    countBot = 0;
+    countYou = 0;
+    document.getElementById("score-you").innerHTML = 0;
+    document.getElementById("score-bot").innerHTML = 0;
+    document.getElementById("img-bot").src = "";
+    document.getElementById("img-you").src = "";
+    document.getElementById("winner").innerHTML = "";
+    document.getElementById("choose-you").style.display = "flex"
+    document.getElementById("img-bot").src = "images/empty.png"
+    document.getElementById("img-you").src = "images/empty.png"
+}
+
+function onClickToggle() {
+    if (document.getElementById("toggledark").value == "light") {
+        document.body.style.background = "#222425";
+        document.body.style.color = "white";
+        document.getElementById("you").style.borderRightColor = "white"
+        document.getElementById("toggledark").value = "dark";
+    } else {
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+        document.getElementById("you").style.borderRightColor = "black"
+        document.getElementById("toggledark").value = "light";        
+    }
+}
